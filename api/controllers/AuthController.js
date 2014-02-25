2caa95fc9e11903d38707fe92b010d2df756d829
glyad <david@logoui.co.il>
2014-02-26 01:32:44 +0200

    Probing

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -15,12 +15,30 @@
  * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
 
+var passport = require('passport');
+
 module.exports = {
     
   authenticate : function (req, res, next) {
 
       return next();
   },
+
+//    // New staff
+//    login: function(req, res){
+//        passport.authenticate('local', function(err, user, info){
+//            if ((err) || (!user)) res.send(err);
+//            req.logIn(user, function(err){
+//                if (err) res.send(err);
+//                return res.send({ message: 'login successful' });
+//            });
+//        })(req, res);
+//    },
+//
+//    logout: function (req,res){
+//        req.logout();
+//        res.send('logout successful');
+//    },
 
 
   /**
